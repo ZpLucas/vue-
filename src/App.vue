3931baa5d93721d7fe992c98491813d6ca4d2a82
zhaoPan <206935824@qq.com>
2018-07-11 11:53:11 +0800

    components is ok

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    App
+    <router-view/>
+    <FooterGuide v-show="$route.meta.isShow"/>
   </div>
 </template>
 
 <script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 export default {
+    mounted(){
+      this.$store.dispatch('getAddress')
+      this.$store.dispatch('getUser')
+    },
+
+    components:{
+      FooterGuide
+    }
 
 }
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
---
+++ src/store/mutation-types.js
@@ -0,0 +1,13 @@
+/*
+* mutation type常量名词模块
+* */
+export const RECEIVE_ADDRESS = 'receive_address'
+export const RECEIVE_CATEGORYS = 'receive_categorys'
+export const RECEIVE_SHOPS = 'receive_shops'
+export const RECEIVE_USER='receive_user'
+export const RESET_USER='reset_user' //重置用户
+
+
+export const RECEIVE_GOODS = 'receive_goods' // 接收商品列表
+export const RECEIVE_RATINGS = 'receive_ratings' // 接收评论列表
+export const RECEIVE_INFO = 'receive_info' // 接收商家信息
